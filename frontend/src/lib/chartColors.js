fa0d2bbdb1f5137867968dea963b0b7308b97474
mchithna <mchithna@gmail.com>
2026-07-05 12:07:54 +0530

    feat: add Design System configuration to frontend

---
+++ frontend/src/lib/chartColors.js
@@ -0,0 +1,7 @@
+const colors = require('tailwindcss/colors');
+
+export const CHART_COLORS = [
+  colors.indigo[500], colors.violet[500], colors.sky[500],
+  colors.emerald[500], colors.amber[500], colors.rose[400],
+  colors.slate[400], colors.teal[500],
+];
