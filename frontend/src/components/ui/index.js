a97b6b2ea16b34e804555766f307915e5669649c
Yasindu Udayanga <yasinduudayanga2003@gmail.com>
2026-07-05 17:58:10 +0530

    feat: create barrel file for centralized UI component exports

---
+++ frontend/src/components/ui/index.js
@@ -0,0 +1,17 @@
+/**
+ * Barrel export for all UI form & input components.
+ *
+ * Usage:
+ *   import { Button, Input, Select } from '@/components/ui';
+ */
+
+export { Button } from './Button';
+export { Input } from './Input';
+export { Textarea } from './Textarea';
+export { Select } from './Select';
+export { Checkbox } from './Checkbox';
+export { RadioGroup, Radio } from './RadioGroup';
+export { Switch } from './Switch';
+export { SearchInput } from './SearchInput';
+export { FileUpload } from './FileUpload';
+export { DateTimeInput } from './DateTimeInput';
